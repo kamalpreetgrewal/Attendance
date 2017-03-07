74a7b80ce563613f05be6428f8380e1e80a6d4fc
Kamalpreet Grewal <grewalkamal005@gmail.com>
2017-03-07 21:46:02 +0530

    Add recyclerview

--- app/src/main/java/com/example/kamal/attendanceapp/activities/MainActivity.java
+++ app/src/main/java/com/example/kamal/attendanceapp/activities/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.kamal.attendanceapp.activities;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 
 import com.example.kamal.attendanceapp.R;
@@ -9,6 +12,11 @@ import com.example.kamal.attendanceapp.R;
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private DrawerLayout drawerLayout;
+    private RecyclerView.Adapter adapter;
+    private ActionBarDrawerToggle barDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
