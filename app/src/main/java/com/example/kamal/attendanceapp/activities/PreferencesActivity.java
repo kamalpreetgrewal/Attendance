45566fde94d9000466970742b717c44a2dc9e73c
Kamalpreet Grewal <grewalkamal005@gmail.com>
2017-03-07 17:42:27 +0530

    Add fragments, activities and packages

--- app/src/main/java/com/example/kamal/attendanceapp/activities/PreferencesActivity.java
+++ app/src/main/java/com/example/kamal/attendanceapp/activities/PreferencesActivity.java
@@ -1,13 +1,13 @@
-package com.example.kamal.attendanceapp;
+package com.example.kamal.attendanceapp.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class PreferencesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_preferences);
     }
 }
